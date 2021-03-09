d15509a1950326a931b01b3a65369a2c92e6dcb2
Jimon_s <22317839+Jimon-s@users.noreply.github.com>
2021-03-10 05:54:14 +0900

    Add formula

---
+++ awsmfa.rb
@@ -0,0 +1,31 @@
+class Awsmfa < Formula
+  desc "Simple cli tool for AWS MFA (multi factor authentication)"
+  homepage "https://github.com/Jimon-s/awsmfa"
+  url "https://github.com/Jimon-s/awsmfa/archive/v1.0.1.tar.gz"
+  sha256 "07934399a243cf5f3a12c5856893e8f5595ca4a0a7d8077027f98d881b3a205a"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # Install awsmfa command
+    system "go", "build"
+    bin.install "awsmfa"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/awsmfa", "completion", "bash")
+    (bash_completion/"awsmfa").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/awsmfa", "completion", "zsh")
+    (zsh_completion/"_awsmfa").write output
+  end
+
+  test do
+    run_output = shell_output("#{bin}/awsmfa --generate-credentials-skeleton get-session-token")
+    want = "[sample-before-mfa]\n\
+aws_access_key_id     = YOUR_ACCESS_KEY_ID_HERE!!!\n\
+aws_secret_access_key = YOUR_SECRET_ACCESS_KEY_HERE!!!\n"
+    assert_match want, run_output
+  end
+end
